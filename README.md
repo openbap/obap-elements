d70ba43e340ad18e48e6b86b5340339e44087b00
Paul Mason <paul.mason@outlook.com>
2020-05-06 13:16:32 +0200

    Updated ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-obap-elements
+# Open Business Application Web Components
+
+> IMPORTANT: The Open Business Application Web Components are a work in progress and subject to major changes until 1.0 release.
+
+The Open Business Application Web Components (OBAP Elements) are a collection of Web Components designed specifically for desktop browser line of business applications and implement a simplified and modified Material Design.
+
+## Components
+
+There's a pretty long list, which you can find [here](README.md).
+
+## Quick Start
+
+### 1) Install
+
+Install the components from NPM:
+
+```
+npm i @obap/obap-elements
+```
+
+### 2) Import the elements that you wish to use
+
+TODO
+
+## Additional Resources
+
+A good starting place is the [LitElement documentation](https://lit-element.polymer-project.org/), and the [Material Web Components](https://github.com/material-components/material-components-web-components) have some useful information (since they're also based on LitElement)
---
+++ test/obap-element.test.js
@@ -0,0 +1,88 @@
+import { html, fixture, expect, nextFrame, defineCE, unsafeStatic } from '@open-wc/testing';
+import { ObapElement } from '../src/obap-element/obap-element.js';
+
+const tagName = defineCE(
+    class extends ObapElement {
+        constructor() {
+            super();
+        }
+    },
+);
+
+const tag = unsafeStatic(tagName);
+
+describe('obap-element', () => {
+    it('can set the disabled state', async () => {
+        const el = await fixture(html`
+            <${tag} disabled></${tag}>
+        `);
+
+        el.disabled = true;
+        expect(el.disabled).to.equal(true);
+
+        el.disabled = false;
+        expect(el.disabled).to.equal(false);
+    });
+
+    it('reflects the disabled property to the attribute', async () => {
+        const el = await fixture(html`
+            <${tag}></${tag}>
+        `);
+
+        el.disabled = true;
+        await nextFrame();
+        expect(el.hasAttribute('disabled')).to.equal(true);
+
+        el.disabled = false;
+        await nextFrame();
+        expect(el.hasAttribute('disabled')).to.equal(false);
+    });
+
+    it('sets the disabled property from the attribute', async () => {
+        const el = await fixture(html`
+            <${tag} disabled></${tag}>
+        `);
+
+        expect(el.disabled).to.equal(true);
+    });
+
+    it('reflects the disabled state to the aria-disabled attribute', async () => {
+        const el = await fixture(html`
+            <${tag}></${tag}>
+        `);
+
+        el.disabled = true;
+        await nextFrame();
+        expect(el.getAttribute('aria-disabled')).to.equal('true');
+
+        el.disabled = false;
+        await nextFrame();
+        expect(el.getAttribute('aria-disabled')).to.equal('false');
+    });
+
+    it('reflects the role property to the attribute', async () => {
+        const el = await fixture(html`
+            <${tag}></${tag}>
+        `);
+
+        el.role = 'button';
+        await nextFrame();
+        expect(el.getAttribute('role')).to.equal('button');
+    });
+
+    it('sets the role property from the attribute', async () => {
+        const el = await fixture(html`
+            <${tag} role="button"></${tag}>
+        `);
+
+        expect(el.role).to.equal('button');
+    });
+
+    it('passes the a11y audit', async () => {
+        const el = await fixture(html`
+            <${tag}></${tag}>
+        `);
+
+        await expect(el).shadowDom.to.be.accessible();
+    });
+});
